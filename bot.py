ab0b4da0dec108dbd9a2fda850ac25e8522be199
ryanpeagle-rntp <ryan_nathan_t_paguila@dlsu.edu.ph>
2025-11-26 06:37:13 +0800

    bot.py no longer does multiple runs

--- bot.py
+++ bot.py
@@ -17,28 +17,25 @@ def main():
     args = parser.parse_args()
     
     # Train the agent
-    count = 0
-    while count <33:
-        count+=1
-        #print(f"\n\nIteration {count}")
+    #print(f"\n\nIteration {count}")
 
-        #print(f"\nTraining agent against {args.cat} cat...")
-        q_table, time = train_bot(
-            cat_name=args.cat,
-            render=args.render
-        )
+    #print(f"\nTraining agent against {args.cat} cat...")
+    q_table, time = train_bot(
+        cat_name=args.cat,
+        render=args.render
+    )
         
-        #print("\nTraining complete! Starting game with trained bot...")
-        #print("Press Q to quit.")
+    #print("\nTraining complete! Starting game with trained bot...")
+    #print("Press Q to quit.")
         
-        # Play using the trained Q-table
-        env = make_env(cat_type=args.cat)
-        caught,moves = play_q_table(env, q_table, max_steps=60, window_title='Cat Chase - Final Trained Bot',move_delay=0.00)
+    # Play using the trained Q-table
+    env = make_env(cat_type=args.cat)
+    caught,moves = play_q_table(env, q_table, max_steps=60, window_title='Cat Chase - Final Trained Bot',move_delay=0.25)
 
-        if caught:
-            print(f"The bot successfully caught the cat! ||| {moves} ||| {time}")
-        else:
-            print("The bot failed to catch the cat this time.")
+    if caught:
+        print(f"The bot successfully caught the cat! ||| {moves} ||| {time}")
+    else:
+        print("The bot failed to catch the cat this time.")
     
 
 if __name__ == "__main__":
